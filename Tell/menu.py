a8798e237bba6afcb18f291eb18afbc32928135e
Valeriya <wal.now@yandex.ru>
2022-12-26 00:39:30 +0300

    g

--- Tell/menu.py
+++ Tell/menu.py
@@ -1,4 +1,8 @@
 import controller
+import operations
+import export_data
+import searchcontact
+import import_data
 
 from telebot import TeleBot, types
 #import os
@@ -12,26 +16,60 @@ dct = {}
 def answer(msg: types.Message):
     dct[msg.from_user.id] = []
     bot.send_message(chat_id=msg.from_user.id, text=f'Здравствуйте. Вы открыли телефонный справочник!')
-    #menu()
+    bot.send_message(chat_id=msg.from_user.id, text=f'1. Добавить новую запись \n 2. Вывод записей на экран \n 3. Скопировать данные в файл?\n 4. Сохранить в нашу базу данных?\n 5. Поиск записи')
+    n = msg.text
+    
+    if n == '1':
+        temp = operations.new_contact()
+    #enter = input('Нажмите Enter для завершения')
+'''
+    elif n == '2':
+        print(
+            'Выберите формат отображения данных: 1 - Построчно; 2 - В одну строку', sep='\n')
+        num = int(input())
+        if num == 1 or num == 2:
+            operations.read_contact(num)
+        enter = input('Нажмите Enter для выхода в меню ')
 
-# def greeting():
-#     print()
-#     print('<<ТЕЛЕФОННЫЙ СПРАВОЧНИК>>')
+    elif n == '3':
+        '''
+        # если вы все данные из своего файла скопируете в файл, 
+        # который укажет пользователь, то это можно считать экспортом
+'''
+        file_name_expott = input(
+            'Нипишите имя файла куда вы хотите скопировать данные: ')
+        print('Выберите формат экспорта данных:',
+              '1 - xml', '2 - json', sep='\n')
+        num_exp = input()
+        if num_exp == '1':
+            export_data.export_to_xml(file_name_expott)
+            print('Данные успешно экспортированы в формате xml!\n')
+        elif num_exp == '2':
+            export_data.csv_to_json(file_name_expott)
+            print(f'Данные успешно сохранены в файл {file_name_expott}.json\n')
+        else:
+            print('Пожалуста, введите номер пункта меню: ')
+            enter = input('Нажмите Enter для выхода в меню ')
+        enter = input('Нажмите Enter для завершения')
 
-''''
-def menu():
-    while True:
-        print('\nМЕНЮ')
-        print('1. Добавить новую запись')  # Работает
-        print('2. Вывод записей на экран')  # Работает
-        print('3. Скопировать данные в файл?')  # надо добавить копирование в json
-        print('4. Сохранить в нашу базу данных?') # надо сделать
-        print('5. Поиск записи')  # работает
-        print('6. Выход\n')   # работает
-        number = input('Выберите пункт меню: ')
-        controller.distribute(number)
-        if number =='6':
-            break
+    elif n == '4':
+        '''
+        # считывание и сохранение, в вашу базу, записей из файла, 
+        # который указал пользователь
+'''
+        num = int(input(
+            'Выберете тип файла, из которого будете брать данные 1 - csv, 2 - json'))
+        if num == 1:
+            arr1 = import_data.copy_cont()
+            import_data.write_csv(arr1)
+        if num == 2:
+            import_data.copy_cont_json()
+        enter = input('Нажмите Enter для завершения')
+
+    elif n == '5':
+        user_search = input('Введите имя для поиска?: ')
+        searchcontact.searchcontact(user_search)
+        enter = input('Нажмите Enter для завершения')     
  '''
 
 #greeting()
