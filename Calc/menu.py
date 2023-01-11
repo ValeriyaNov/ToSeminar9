0360265de1468ace17d4402e37090b89d5b3093c
Valeriya <wal.now@yandex.ru>
2023-01-11 22:22:28 +0300

    m

--- Calc/menu.py
+++ Calc/menu.py
@@ -38,6 +38,13 @@ def send_welcome(message):
     elif text == '- - -':
         bot.register_next_step_handler(message, subtraction_complex_numbers)
         bot.reply_to(message, m.MESSAGE_1)
+
+    elif text == '***':
+        bot.register_next_step_handler(message, mult_complex_numbers)
+        bot.reply_to(message, m.MESSAGE_1)
+    elif text == '///':
+        bot.register_next_step_handler(message, dev_complex_numbers)
+        bot.reply_to(message, m.MESSAGE_1)    
     else:
         bot.reply_to(message, text='Вы прислали: ' + text +
                         ', а должны были арифметическое действие')
@@ -202,8 +209,6 @@ def subtraction_complex_numbers(msg):
         res = f'{jsum}j{sum1}'
     else:
         res = f'{jsum}j+{sum1}'
-        
-    
 
     bot.send_message(chat_id=msg.from_user.id, text=f'Результат вычитания {res}')
     bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)  
@@ -218,6 +223,149 @@ def devision(msg):
 def mult(msg):
     a, b = map(float, msg.text.split())
     bot.send_message(chat_id=msg.from_user.id, text=f'Результат умножения {a * b}')
+    bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
+
+
+def mult_complex_numbers(msg):
+    c = list(msg.text.split())
+    x = c[0]
+    y = c[1]
+    if x[-1] == 'j':
+        num = 0
+        ncom2 = int(x[:-1])
+    if y[-1] == 'j':
+        numm = 0 
+        ncom4 = int(y[:-1])
+        
+    if x[0]=='-':
+        z = -1
+    else:
+        z = 1
+    if y[0]=='-':
+        zz = -1
+    else:
+        zz = 1
+    if x[0] == '-' or x[0]=='+':
+        x = x[1:]
+
+    if y[0] == '-' or y[0]=='+':
+        y = y[1:]  
+            
+            
+    if '+' in x:
+        ncom1 = x.rpartition('+')[0] 
+        ncom1 = ncom1[:-1]
+            
+        ncom2 = int(ncom1)*z
+            
+        cc = x.rpartition('+')[1] 
+        num = int(x.rpartition('+')[2])
+        if cc == '-':
+            num = (-1)*num
+    elif '-' in x:
+        ncom1 = x.rpartition('-')[0] 
+        ncom1 = ncom1[:-1]
+            
+        ncom2 = int(ncom1)*z
+            
+        cc = x.rpartition('-')[1] 
+        num = int(x.rpartition('-')[2])*(-1) 
+            
+            
+    if '+' in y:
+        ncom3 = y.rpartition('+')[0]
+        ncom3 = ncom3[:-1]
+        ncom4 = int(ncom3)*zz
+        ccc = y.rpartition('+')[1] 
+        numm = int(y.rpartition('+')[2])
+
+    elif '-' in y:
+        ncom3 = y.rpartition('-')[0]
+        ncom3 = ncom3[:-1]
+        ncom4 = int(ncom3)*zz
+        ccc = y.rpartition('-')[1] 
+        numm = int(y.rpartition('-')[2])*(-1) 
+            
+    jmult = num*numm - ncom2*ncom4
+    mult = num*ncom4 + numm*ncom2
+    if mult<0:
+        res = f'{jmult}j{mult}'
+    else:
+        res = f'{jmult}j+{mult}'
+            
+    bot.send_message(chat_id=msg.from_user.id, text=f'Результат умножения {res}')
+    bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
+    
+
+def dev_complex_numbers(msg):
+    c = list(msg.text.split())
+    x = c[0]
+    y = c[1]
+    if x[-1] == 'j':
+        num = 0
+        ncom2 = int(x[:-1])
+    if y[-1] == 'j':
+        numm = 0 
+        ncom4 = int(y[:-1])
+        
+    if x[0]=='-':
+        z = -1
+    else:
+        z = 1
+    if y[0]=='-':
+        zz = -1
+    else:
+        zz = 1
+    if x[0] == '-' or x[0]=='+':
+        x = x[1:]
+
+    if y[0] == '-' or y[0]=='+':
+            y = y[1:]  
+            
+            
+    if '+' in x:
+        ncom1 = x.rpartition('+')[0] 
+        ncom1 = ncom1[:-1]
+            
+        ncom2 = int(ncom1)*z
+            
+        cc = x.rpartition('+')[1] 
+        num = int(x.rpartition('+')[2])
+        if cc == '-':
+            num = (-1)*num
+    elif '-' in x:
+        ncom1 = x.rpartition('-')[0] 
+        ncom1 = ncom1[:-1]
+            
+        ncom2 = int(ncom1)*z
+            
+        cc = x.rpartition('-')[1] 
+        num = int(x.rpartition('-')[2])*(-1) 
+        
+        
+    if '+' in y:
+        ncom3 = y.rpartition('+')[0]
+        ncom3 = ncom3[:-1]
+        ncom4 = int(ncom3)*zz
+        ccc = y.rpartition('+')[1] 
+        numm = int(y.rpartition('+')[2])
+
+    elif '-' in y:
+        ncom3 = y.rpartition('-')[0]
+        ncom3 = ncom3[:-1]
+        ncom4 = int(ncom3)*zz
+        ccc = y.rpartition('-')[1] 
+        numm = int(y.rpartition('-')[2])*(-1) 
+            
+    dev = (num*numm + ncom2*ncom4)/(numm**2 + ncom4**2)
+    jdev = (ncom2*numm - num*ncom4)/(numm**2 + ncom4**2)
+    num*ncom4 + numm*ncom2
+    if dev<0:
+        res = f'{jdev}j{dev}'
+    else:
+        res = f'{jdev}j+{dev}'
+        
+    bot.send_message(chat_id=msg.from_user.id, text=f'Результат деления {res}')
     bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
 
 
