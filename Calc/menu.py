0070bd1355d2e3e3e1ad264e511f3219f26fbd7b
Valeriya <wal.now@yandex.ru>
2023-01-12 16:08:56 +0300

    l

--- Calc/menu.py
+++ Calc/menu.py
@@ -6,7 +6,7 @@ import datetime
 import codecs
 os.chdir(os.path.dirname(__file__))
 
-token = '5615159193:AAFAm4a5YKA3w2EhtlvwS9qkDEzL0jqalUo'
+token = ''
 bot = TeleBot(token)
 
 @bot.message_handler(commands=m.USER_COMMANDS)
@@ -34,20 +34,6 @@ def send_welcome(message):
     elif text == '*':
         bot.register_next_step_handler(message, mult)
         bot.reply_to(message, m.MESSAGE_1)
-    # elif text == '+++':
-    #     bot.register_next_step_handler(message, sum_complex_numbers)
-    #     bot.reply_to(message, m.MESSAGE_1)
-    # elif text == '- - -':
-    #     bot.register_next_step_handler(message, subtraction_complex_numbers)
-    #     bot.reply_to(message, m.MESSAGE_1)
-
-    # elif text == '***':
-    #     bot.register_next_step_handler(message, mult_complex_numbers)
-    #     bot.reply_to(message, m.MESSAGE_1)
-    # elif text == '///':
-    #     bot.register_next_step_handler(message, dev_complex_numbers)
-    #     bot.reply_to(message, m.MESSAGE_1)  
-
     elif text == 'log':
         bot.register_next_step_handler(message, logir_print)
         bot.reply_to(message, m.MESSAGE_3)
@@ -65,7 +51,6 @@ def logir(message):
             f'\n Chat {message.chat.id} User: {message.from_user.first_name} Data: {dtn} Message 1: {message.text}')
 
 
-
 def logir_print(msg):
         
     bot.send_document(chat_id=msg.from_user.id, document=open(f'log', 'rb'))
@@ -75,16 +60,20 @@ def chek(num):
     if 'j' in num:
         num = translation.transl(num)
     elif num.isdigit():
+        num = int(num)
+    elif '.' in num:
+        num = float(num)
+    elif ',' in num:
+        num = num.replace(',', '.')  
         num = float(num)
     return num
     
-
 
 def sum(msg):
     k = msg.text.split()
     a = chek(k[0])
     b = chek(k[1])
-    #a, b = map(float, msg.text.split())
+    
     answer = a + b
     bot.send_message(chat_id=msg.from_user.id, text=f'Результат сложения {answer}')
     bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
@@ -102,7 +91,6 @@ def subtraction(msg):
     with codecs.open('log', 'a', encoding='utf-8') as file:
         file.writelines(f'Message 2 {msg.text} , Answer {answer}')
     
-
     
 def devision(msg):
     k = msg.text.split()
@@ -110,6 +98,7 @@ def devision(msg):
     b = chek(k[1])
     if b == 0:
         bot.send_message(chat_id=msg.from_user.id, text=f'На 0 делить нельзя!')
+        exit()
     answer = a / b
     bot.send_message(chat_id=msg.from_user.id, text=f'Результат деления {answer}')
     bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
