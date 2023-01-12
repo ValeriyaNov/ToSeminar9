94ee196dabf5d3c3bb77631ff11c84b93b3cce63
Valeriya <wal.now@yandex.ru>
2023-01-12 15:00:56 +0300

    d

--- Calc/menu.py
+++ Calc/menu.py
@@ -1,6 +1,7 @@
 import os
 from telebot import TeleBot, types
 import main as m
+import translation
 import datetime
 import codecs
 os.chdir(os.path.dirname(__file__))
@@ -33,19 +34,19 @@ def send_welcome(message):
     elif text == '*':
         bot.register_next_step_handler(message, mult)
         bot.reply_to(message, m.MESSAGE_1)
-    elif text == '+++':
-        bot.register_next_step_handler(message, sum_complex_numbers)
-        bot.reply_to(message, m.MESSAGE_1)
-    elif text == '- - -':
-        bot.register_next_step_handler(message, subtraction_complex_numbers)
-        bot.reply_to(message, m.MESSAGE_1)
+    # elif text == '+++':
+    #     bot.register_next_step_handler(message, sum_complex_numbers)
+    #     bot.reply_to(message, m.MESSAGE_1)
+    # elif text == '- - -':
+    #     bot.register_next_step_handler(message, subtraction_complex_numbers)
+    #     bot.reply_to(message, m.MESSAGE_1)
 
-    elif text == '***':
-        bot.register_next_step_handler(message, mult_complex_numbers)
-        bot.reply_to(message, m.MESSAGE_1)
-    elif text == '///':
-        bot.register_next_step_handler(message, dev_complex_numbers)
-        bot.reply_to(message, m.MESSAGE_1)  
+    # elif text == '***':
+    #     bot.register_next_step_handler(message, mult_complex_numbers)
+    #     bot.reply_to(message, m.MESSAGE_1)
+    # elif text == '///':
+    #     bot.register_next_step_handler(message, dev_complex_numbers)
+    #     bot.reply_to(message, m.MESSAGE_1)  
 
     elif text == 'log':
         bot.register_next_step_handler(message, logir_print)
@@ -70,9 +71,20 @@ def logir_print(msg):
     bot.send_document(chat_id=msg.from_user.id, document=open(f'log', 'rb'))
              
 
+def chek(num):
+    if 'j' in num:
+        num = translation.transl(num)
+    elif num.isdigit():
+        num = float(num)
+    return num
+    
+
 
 def sum(msg):
-    a, b = map(float, msg.text.split())
+    k = msg.text.split()
+    a = chek(k[0])
+    b = chek(k[1])
+    #a, b = map(float, msg.text.split())
     answer = a + b
     bot.send_message(chat_id=msg.from_user.id, text=f'Результат сложения {answer}')
     bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
@@ -80,84 +92,10 @@ def sum(msg):
         file.writelines(f'Message 2 {msg.text} , Answer {answer}')
 
 
-def sum_complex_numbers(msg):
-    c = list(msg.text.split())
-    x = c[0]
-    y = c[1]
-    if x[-1] == 'j':
-        num = 0
-        ncom2 = int(x[:-1])
-    if y[-1] == 'j':
-        numm = 0 
-        ncom4 = int(y[:-1])
-    
-    if x[0]=='-':
-        z = -1
-    else:
-        z = 1
-    if y[0]=='-':
-        zz = -1
-    else:
-        zz = 1
-    if x[0] == '-' or x[0]=='+':
-        x = x[1:]
-
-    if y[0] == '-' or y[0]=='+':
-        y = y[1:]  
-        
-        
-    if '+' in x:
-        ncom1 = x.rpartition('+')[0] 
-        ncom1 = ncom1[:-1]
-        
-        ncom2 = int(ncom1)*z
-        
-        cc = x.rpartition('+')[1] 
-        num = int(x.rpartition('+')[2])
-        if cc == '-':
-            num = (-1)*num
-    elif '-' in x:
-        ncom1 = x.rpartition('-')[0] 
-        ncom1 = ncom1[:-1]
-        
-        ncom2 = int(ncom1)*z
-        
-        cc = x.rpartition('-')[1] 
-        num = int(x.rpartition('-')[2])*(-1) 
-        
-        
-    if '+' in y:
-        ncom3 = y.rpartition('+')[0]
-        ncom3 = ncom3[:-1]
-        ncom4 = int(ncom3)*zz
-        ccc = y.rpartition('+')[1] 
-        numm = int(y.rpartition('+')[2])
-
-    elif '-' in y:
-        ncom3 = y.rpartition('-')[0]
-        ncom3 = ncom3[:-1]
-        ncom4 = int(ncom3)*zz
-        ccc = y.rpartition('-')[1] 
-        numm = int(y.rpartition('-')[2])*(-1) 
-        
-    jsum = ncom2+ncom4
-    sum1 = num + numm
-    if sum1<0:
-        res = f'{jsum}j{sum1}'
-    else:
-        res = f'{jsum}j+{sum1}'
-        
-    answer = res
-
-    bot.send_message(chat_id=msg.from_user.id, text=f'Результат сложения {answer}')
-    bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2) 
-    with codecs.open('log', 'a', encoding='utf-8') as file:
-        file.writelines(f'Message 2 {msg.text} , Answer {answer}')
-
-
-
 def subtraction(msg):
-    a, b = map(float, msg.text.split())
+    k = msg.text.split()
+    a = chek(k[0])
+    b = chek(k[1])
     answer = a - b
     bot.send_message(chat_id=msg.from_user.id, text=f'Результат вычитания {answer}')
     bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
@@ -165,81 +103,13 @@ def subtraction(msg):
         file.writelines(f'Message 2 {msg.text} , Answer {answer}')
     
 
-def subtraction_complex_numbers(msg):
-    c = list(msg.text.split())
-    x = c[0]
-    y = c[1]
-    if x[-1] == 'j':
-        num = 0
-        ncom2 = int(x[:-1])
-    if y[-1] == 'j':
-        numm = 0 
-        ncom4 = int(y[:-1])
-    
-    if x[0]=='-':
-        z = -1
-    else:
-        z = 1
-    if y[0]=='-':
-        zz = -1
-    else:
-        zz = 1
-    if x[0] == '-' or x[0]=='+':
-        x = x[1:]
-
-    if y[0] == '-' or y[0]=='+':
-        y = y[1:]  
-        
-        
-    if '+' in x:
-        ncom1 = x.rpartition('+')[0] 
-        ncom1 = ncom1[:-1]
-        
-        ncom2 = int(ncom1)*z
-        
-        cc = x.rpartition('+')[1] 
-        num = int(x.rpartition('+')[2])
-        if cc == '-':
-            num = (-1)*num
-    elif '-' in x:
-        ncom1 = x.rpartition('-')[0] 
-        ncom1 = ncom1[:-1]
-        
-        ncom2 = int(ncom1)*z
-        
-        cc = x.rpartition('-')[1] 
-        num = int(x.rpartition('-')[2])*(-1) 
-        
-        
-    if '+' in y:
-        ncom3 = y.rpartition('+')[0]
-        ncom3 = ncom3[:-1]
-        ncom4 = int(ncom3)*zz
-        ccc = y.rpartition('+')[1] 
-        numm = int(y.rpartition('+')[2])
-
-    elif '-' in y:
-        ncom3 = y.rpartition('-')[0]
-        ncom3 = ncom3[:-1]
-        ncom4 = int(ncom3)*zz
-        ccc = y.rpartition('-')[1] 
-        numm = int(y.rpartition('-')[2])*(-1) 
-        
-    jsum = ncom2 - ncom4
-    sum1 = num - numm
-    if sum1<0:
-        res = f'{jsum}j{sum1}'
-    else:
-        res = f'{jsum}j+{sum1}'
-    answer = res
-    bot.send_message(chat_id=msg.from_user.id, text=f'Результат вычитания {answer}')
-    bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2) 
-    with codecs.open('log', 'a', encoding='utf-8') as file:
-        file.writelines(f'Message 2 {msg.text} , Answer {answer}')
-
     
 def devision(msg):
-    a, b = map(float, msg.text.split())
+    k = msg.text.split()
+    a = chek(k[0])
+    b = chek(k[1])
+    if b == 0:
+        bot.send_message(chat_id=msg.from_user.id, text=f'На 0 делить нельзя!')
     answer = a / b
     bot.send_message(chat_id=msg.from_user.id, text=f'Результат деления {answer}')
     bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
@@ -248,156 +118,11 @@ def devision(msg):
     
     
 def mult(msg):
-    a, b = map(float, msg.text.split())
+    k = msg.text.split()
+    a = chek(k[0])
+    b = chek(k[1])
     answer = a * b
     bot.send_message(chat_id=msg.from_user.id, text=f'Результат умножения {answer}')
-    bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
-    with codecs.open('log', 'a', encoding='utf-8') as file:
-        file.writelines(f'Message 2 {msg.text} , Answer {answer}')
-
-
-def mult_complex_numbers(msg):
-    c = list(msg.text.split())
-    x = c[0]
-    y = c[1]
-    if x[-1] == 'j':
-        num = 0
-        ncom2 = int(x[:-1])
-    if y[-1] == 'j':
-        numm = 0 
-        ncom4 = int(y[:-1])
-        
-    if x[0]=='-':
-        z = -1
-    else:
-        z = 1
-    if y[0]=='-':
-        zz = -1
-    else:
-        zz = 1
-    if x[0] == '-' or x[0]=='+':
-        x = x[1:]
-
-    if y[0] == '-' or y[0]=='+':
-        y = y[1:]  
-            
-            
-    if '+' in x:
-        ncom1 = x.rpartition('+')[0] 
-        ncom1 = ncom1[:-1]
-            
-        ncom2 = int(ncom1)*z
-            
-        cc = x.rpartition('+')[1] 
-        num = int(x.rpartition('+')[2])
-        if cc == '-':
-            num = (-1)*num
-    elif '-' in x:
-        ncom1 = x.rpartition('-')[0] 
-        ncom1 = ncom1[:-1]
-            
-        ncom2 = int(ncom1)*z
-            
-        cc = x.rpartition('-')[1] 
-        num = int(x.rpartition('-')[2])*(-1) 
-            
-            
-    if '+' in y:
-        ncom3 = y.rpartition('+')[0]
-        ncom3 = ncom3[:-1]
-        ncom4 = int(ncom3)*zz
-        ccc = y.rpartition('+')[1] 
-        numm = int(y.rpartition('+')[2])
-
-    elif '-' in y:
-        ncom3 = y.rpartition('-')[0]
-        ncom3 = ncom3[:-1]
-        ncom4 = int(ncom3)*zz
-        ccc = y.rpartition('-')[1] 
-        numm = int(y.rpartition('-')[2])*(-1) 
-            
-    mult = num*numm - ncom2*ncom4
-    jmult = num*ncom4 + numm*ncom2
-    if mult<0:
-        res = f'{jmult}j{mult}'
-    else:
-        res = f'{jmult}j+{mult}'
-    answer = res      
-    bot.send_message(chat_id=msg.from_user.id, text=f'Результат умножения {answer}')
-    bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
-    with codecs.open('log', 'a', encoding='utf-8') as file:
-        file.writelines(f'Message 2 {msg.text} , Answer {answer}')
-    
-
-def dev_complex_numbers(msg):
-    c = list(msg.text.split())
-    x = c[0]
-    y = c[1]
-    if x[-1] == 'j':
-        num = 0
-        ncom2 = int(x[:-1])
-    if y[-1] == 'j':
-        numm = 0 
-        ncom4 = int(y[:-1])
-        
-    if x[0]=='-':
-        z = -1
-    else:
-        z = 1
-    if y[0]=='-':
-        zz = -1
-    else:
-        zz = 1
-    if x[0] == '-' or x[0]=='+':
-        x = x[1:]
-
-    if y[0] == '-' or y[0]=='+':
-            y = y[1:]  
-            
-            
-    if '+' in x:
-        ncom1 = x.rpartition('+')[0] 
-        ncom1 = ncom1[:-1]
-            
-        ncom2 = int(ncom1)*z
-            
-        cc = x.rpartition('+')[1] 
-        num = int(x.rpartition('+')[2])
-        if cc == '-':
-            num = (-1)*num
-    elif '-' in x:
-        ncom1 = x.rpartition('-')[0] 
-        ncom1 = ncom1[:-1]
-            
-        ncom2 = int(ncom1)*z
-            
-        cc = x.rpartition('-')[1] 
-        num = int(x.rpartition('-')[2])*(-1) 
-        
-        
-    if '+' in y:
-        ncom3 = y.rpartition('+')[0]
-        ncom3 = ncom3[:-1]
-        ncom4 = int(ncom3)*zz
-        ccc = y.rpartition('+')[1] 
-        numm = int(y.rpartition('+')[2])
-
-    elif '-' in y:
-        ncom3 = y.rpartition('-')[0]
-        ncom3 = ncom3[:-1]
-        ncom4 = int(ncom3)*zz
-        ccc = y.rpartition('-')[1] 
-        numm = int(y.rpartition('-')[2])*(-1) 
-            
-    dev = (num*numm + ncom2*ncom4)/(numm**2 + ncom4**2)
-    jdev = (ncom2*numm - num*ncom4)/(numm**2 + ncom4**2)
-    num*ncom4 + numm*ncom2
-    if dev<0:
-        res = f'{jdev}j{dev}'
-    else:
-        res = f'{jdev}j+{dev}'
-    answer = res    
-    bot.send_message(chat_id=msg.from_user.id, text=f'Результат деления {answer}')
     bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_2)
     with codecs.open('log', 'a', encoding='utf-8') as file:
         file.writelines(f'Message 2 {msg.text} , Answer {answer}')
